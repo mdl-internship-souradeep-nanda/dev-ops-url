b22295267413d1fd11626119d9ede961eb7a13b8
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-05 14:11:50 +0530

    Updated seeders

--- seeders/20180305042022-urlseeds.js
+++ seeders/20180305042022-urlseeds.js
@@ -3,16 +3,40 @@ const md5 = require('md5');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     const urls = [];
-    for (let i = 0; i < 1e+6; i += 1) {
+    const map = {};
+
+    const totalEntries = 1e+6;
+    const shortUrlLength = 6;
+
+    for (let i = 0; i < totalEntries; i += 1) {
       const longurl = `http://givemepotatoes.com/${i}`;
-      const shorturl = md5(longurl).substring(0, 6);
+      const hash = md5(longurl);
+
+      // if there is a hash collision then get the next 6
+      // characters of the hash
+      let j = 0;
+      while (map[hash.substring(j, j + shortUrlLength)] !== undefined) {
+        j += shortUrlLength;
+        if (j + shortUrlLength > hash.length) {
+          console.log('FUCK');
+          break;
+        }
+      }
+
+      // Mark the hash as taken in the hashmap
+      map[hash.substring(j, j + shortUrlLength)] = true;
+      const shorturl = hash.substring(j, j + shortUrlLength);
+
       urls.push({
         longurl, shorturl, createdAt: new Date(), updatedAt: new Date(),
       });
+
+      if (i % 1e+4 === 0) {
+        console.log(i);
+      }
     }
 
-    // console.log(urls);
-
+    console.log('Bulk inserting');
     return queryInterface.bulkInsert('shorturls', urls, {});
   },
 
