4843ffc1ee2243dc2498f459a260780c01b6f656
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-05 15:36:48 +0530

    Handled hash collision

--- src/routes/write.js
+++ src/routes/write.js
@@ -2,18 +2,50 @@ const md5 = require('md5');
 
 const models = require('../../models');
 
+const createNewRecord = (longurl, hash, index) => {
+  const shorturl = hash.substring(index, index + 6);
+  // console.log('shorturl', hash, index);
+  return models.shorturls.findOne({
+    where: {
+      shorturl,
+    },
+  })
+    .then((result) => {
+      if (result === null) {
+        return models.shorturls.upsert({ longurl, shorturl });
+      }
+      return createNewRecord(longurl, hash, index + 6);
+    });
+};
+
 module.exports = [
   {
     path: '/write',
     method: 'GET',
     handler: (request, response) => {
       const { longurl } = request.query;
-      const shorturl = md5(longurl).substring(0, 6);
-      models.shorturls.upsert({
-        longurl,
-        shorturl,
-      }).then(() => {
-        response({ shorturl });
+      const hash = md5(longurl);
+      // console.log('write', hash);
+      models.shorturls.findOne({
+        where: {
+          longurl,
+        },
+      }).then((result) => {
+        if (result !== null) {
+          // If longurl is present in db, just return the shorturl
+          response({ shorturl: result.shorturl });
+        } else {
+          // Create a new entry with unique shorturl
+          createNewRecord(longurl, hash, 0)
+            .then(() => models.shorturls.findOne({
+              where: {
+                longurl,
+              },
+            }))
+            .then((secondResult) => {
+              response({ shorturl: secondResult.shorturl });
+            });
+        }
       });
     },
   },
