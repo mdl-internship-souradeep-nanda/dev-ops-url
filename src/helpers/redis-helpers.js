86b0bd8fd8331e8e66af38e83f0013d048dabac9
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-07 15:37:17 +0530

    refactor redis helpers

--- src/helpers/redis-helpers.js
+++ src/helpers/redis-helpers.js
@@ -3,11 +3,8 @@ const redis = require('redis');
 const client = redis.createClient();
 const { promisify } = require('util');
 
-const getAsync = promisify(client.get).bind(client);
-const setAsync = promisify(client.set).bind(client);
-
-const getFromRedis = key => getAsync(key);
-const storeIntoRedis = (key, value) => setAsync(key, value);
+const getFromRedis = promisify(client.get).bind(client);
+const storeIntoRedis = promisify(client.set).bind(client);
 
 module.exports = {
   getFromRedis,
