2c9a25de1eec832ac660c9536f49fb248fecb77a
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-07 08:30:38 +0530

    Added tests to make coverage 100%

--- test/routes/write.test.js
+++ test/routes/write.test.js
@@ -5,45 +5,68 @@ const server = require('../../src/index');
 const models = require('../../models');
 
 describe('The write route should take an url and', () => {
-  // it('return its md5 hash', (done) => {
-  //   const longurl = 'http://longurl.com';
-  //   const expectedHash = md5(longurl).substring(0, 6);
-  //   supertest(server.listener)
-  //     .get('/write')
-  //     .query({ longurl })
-  //     .expect(200)
-  //     .then((res) => {
-  //       const { shorturl } = res.body;
-  //       expect(shorturl).toBe(expectedHash);
-  //       done();
-  //     });
-  // });
-  // it('store its md5 hash in db', (done) => {
-  //   const longurl = 'http://longurl.com';
-  //   const expectedHash = md5(longurl).substring(0, 6);
-  //   models.shorturls.destroy({ truncate: true })
-  //     .then(() => supertest(server.listener)
-  //       .get('/write')
-  //       .query({ longurl })
-  //       .expect(200)
-  //       .then(() => { }))
-  //     .then(() => models.shorturls.findOne({
-  //       where: {
-  //         longurl,
-  //       },
-  //     }).then((result) => {
-  //       expect(result).toBeTruthy();
-  //       expect(result.shorturl).toBe(expectedHash);
-  //       done();
-  //     }));
-  // });
+  it('return its md5 hash', (done) => {
+    const longurl = 'http://longurl.com';
+    const expectedHash = md5(longurl).substring(0, 6);
+    supertest(server.listener)
+      .get('/write')
+      .query({ longurl })
+      .expect(200)
+      .then((res) => {
+        const { shorturl } = res.body;
+        expect(shorturl).toBe(expectedHash);
+        done();
+      });
+  });
+  it('store its md5 hash in db', (done) => {
+    const longurl = 'http://longurl.com';
+    const expectedHash = md5(longurl).substring(0, 6);
+    models.shorturls.destroy({ truncate: true })
+      .then(() => supertest(server.listener)
+        .get('/write')
+        .query({ longurl })
+        .expect(200)
+        .then(() => { }))
+      .then(() => models.shorturls.findOne({
+        where: {
+          longurl,
+        },
+      }).then((result) => {
+        expect(result).toBeTruthy();
+        expect(result.shorturl).toBe(expectedHash);
+        done();
+      }));
+  });
+  it('fetch url from db if it already exists', (done) => {
+    const longurl = 'http://givemepotatoes.com/7392';
+    const expectedHash = md5(longurl).substring(0, 6);
+
+    models.shorturls.destroy({ truncate: true })
+      .then(() => supertest(server.listener)
+        .get('/write')
+        .query({ longurl })
+        .expect(200)
+        .then((res) => {
+          const { shorturl } = res.body;
+          expect(shorturl).toBe(expectedHash);
+        }))
+      .then(() => supertest(server.listener)
+        .get('/write')
+        .query({ longurl })
+        .expect(200)
+        .then((res) => {
+          const { shorturl } = res.body;
+          expect(shorturl).toBe(expectedHash);
+          done();
+        }));
+  });
   it('handle hash collision if longurl does not match', (done) => {
     const longurl1 = 'http://givemepotatoes.com/7392';
     const longurl2 = 'http://givemepotatoes.com/930';
-    // const expectedHash = md5(longurl1);
-    // console.log(expectedHash);
+
     // Sanity check
-    expect(md5(longurl1).substring(0, 6)).toBe(md5(longurl2).substring(0, 6));
+    expect(md5(longurl1).substring(0, 6))
+      .toBe(md5(longurl2).substring(0, 6));
 
     models.shorturls.destroy({ truncate: true })
       .then(() => supertest(server.listener)
