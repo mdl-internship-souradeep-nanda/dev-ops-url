e925822dbe2804dcbe36b49f212e3a204b55cf45
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-08 09:16:21 +0530

    redisified read route

--- src/routes/read.js
+++ src/routes/read.js
@@ -1,4 +1,8 @@
 const models = require('../../models');
+const {
+  getFromRedis,
+  storeIntoRedis,
+} = require('../../src/helpers/redis-helpers');
 
 module.exports = [
   {
@@ -7,15 +11,34 @@ module.exports = [
     handler: (request, response) => {
       const { shorturl } = request.query;
 
-      models.shorturls.findOne({
-        where: {
-          shorturl,
-        },
-      }).then((obj) => {
-        response({ longurl: obj.longurl });
-      }).catch(() => {
-        response({ longurl: '' });
-      });
+      // Check if data is in redis store
+      getFromRedis(shorturl)
+        .then((longurl) => {
+          if (longurl === null) {
+            // If data does not exist in redis,
+            // then check db
+            models.shorturls.findOne({
+              where: {
+                shorturl,
+              },
+            }).then((obj) => {
+              if (obj !== null) {
+                // If data is found in db,
+                // store it into redis
+                storeIntoRedis(shorturl, obj.longurl)
+                  .then(() => {
+                    response({ longurl: obj.longurl });
+                  });
+              } else {
+                // Data does not exist in DB
+                response({ longurl: '' });
+              }
+            });
+          } else {
+            // Data exists in redis cache
+            response({ longurl });
+          }
+        });
     },
   },
 ];
--- test/routes/read.test.js
+++ test/routes/read.test.js
@@ -4,31 +4,62 @@ const md5 = require('md5');
 const server = require('../../src/index');
 const models = require('../../models');
 
-describe('The read route should', () => {
-  it('take a short url and return the longurl if it exists in db', (done) => {
+const {
+  storeIntoRedis,
+  redisFlushdb,
+} = require('../../src/helpers/redis-helpers');
+
+describe('The read route should take short url and return long url when', () => {
+  it('the url is in redis store', (done) => {
+    const longurl = 'http://longurl.com';
+    const shorturl = md5(longurl).substring(0, 6);
+    redisFlushdb()
+      .then(() => storeIntoRedis(shorturl, longurl))
+      .then(() => supertest(server.listener)
+        .get('/read')
+        .query({ shorturl })
+        .expect(200)
+        .then((res) => {
+          expect(res.body.longurl).toBe(longurl);
+          done();
+        }));
+  });
+  it('the url is not in redis store but in db', (done) => {
     const longurl = 'http://longurl.com';
     const shorturl = md5(longurl).substring(0, 6);
     models.shorturls.upsert({
       longurl, shorturl,
-    }).then(() => supertest(server.listener)
-      .get('/read')
-      .query({ shorturl })
-      .expect(200)
-      .then((res) => {
-        expect(res.body.longurl).toBe(longurl);
-        done();
-      }));
+    })
+      .then(() => redisFlushdb())
+      .then(() => supertest(server.listener)
+        .get('/read')
+        .query({ shorturl })
+        .expect(200)
+        .then((res) => {
+          expect(res.body.longurl).toBe(longurl);
+          done();
+        }));
   });
-  it('take a short url and return empty string if it does not exist', (done) => {
+});
+
+describe('The read route should take short url and return empty string when', () => {
+  it('the shorturl is not in either database', (done) => {
     const longurl = 'http://doesnotexist.com';
     const shorturl = md5(longurl).substring(0, 6);
-    supertest(server.listener)
-      .get('/read')
-      .query({ shorturl })
-      .expect(200)
-      .then((res) => {
-        expect(res.body.longurl).toBe('');
-        done();
-      });
+
+    models.shorturls.destroy({
+      where: {
+        shorturl,
+      },
+    })
+      .then(() =>
+        supertest(server.listener)
+          .get('/read')
+          .query({ shorturl })
+          .expect(200)
+          .then((res) => {
+            expect(res.body.longurl).toBe('');
+            done();
+          }));
   });
 });
